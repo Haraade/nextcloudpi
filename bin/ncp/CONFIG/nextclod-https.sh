cb9196a63ac2f84c4de1f3d1eeb4f524df00438c
Haraade <feriemobiltelefon@gmail.com>
2020-04-24 15:13:38 +0200

    Update and rename nc-httpsonly.sh to nextclod-https.sh

--- bin/ncp/CONFIG/nextclod-https.sh
+++ bin/ncp/CONFIG/nextclod-https.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# HTTPS rewrite configuration script for NextCloudPi
+# HTTPS rewrite configuration script for NETTSERVER
 #
-# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
-# GPL licensed (see end of file) * Use at your own risk!
+# GPL licensed - end of file
 #
-# More at https://ownyourbits.com/2017/03/13/nextcloudpi-gets-nextcloudpi-config/
 #
-
 
 configure()
 {
