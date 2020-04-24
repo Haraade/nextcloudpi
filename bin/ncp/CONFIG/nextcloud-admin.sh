8cf104fe87c7f172f008eee89d76761576f05923
Haraade <feriemobiltelefon@gmail.com>
2020-04-24 15:05:49 +0200

    Update and rename nc-admin.sh to nextcloud-admin.sh

--- bin/ncp/CONFIG/nextcloud-admin.sh
+++ bin/ncp/CONFIG/nextcloud-admin.sh
@@ -2,13 +2,9 @@
 
 # Change password for the Nextcloud admin user
 #
-# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
-# GPL licensed (see end of file) * Use at your own risk!
+# GPL licensed - end of file
 #
-# More at: https://ownyourbits.com
 #
-
-
 
 configure()
 {
