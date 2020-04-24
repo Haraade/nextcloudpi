4f1188d80f248f5665a85b0832a341241bfadfad
Haraade <feriemobiltelefon@gmail.com>
2020-04-24 17:49:22 +0200

    Update and rename nc-ramlogs.sh to nettserver-ramlogs.sh

--- bin/ncp/SYSTEM/nettserver-ramlogs.sh
+++ bin/ncp/SYSTEM/nettserver-ramlogs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Data dir configuration script for NextCloudPi
+# Ramlog configuration script for NETTSERVER
 #
-# Copyleft 2017 by Ignacio Nunez Hernanz <nacho _a_t_ ownyourbits _d_o_t_ com>
-# GPL licensed (see end of file) * Use at your own risk!
+# GPL licensed - end of file
 #
-# More at https://ownyourbits.com/
 #
 
 
@@ -37,7 +35,7 @@ configure()
   [[ $ACTIVE != "yes" ]] && {
     systemctl disable "$ramlog"
     systemctl stop    "$ramlog"
-    echo "Logs in SD. Reboot to take effect"
+    echo "Logs in eMMC Module. Reboot to take effect"
     return
   }
 
